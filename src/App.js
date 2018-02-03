7e69cd681c6c2478b140af92f9424582dfc8e3c8
randomness38 <randomness.jay@gmail.com>
2018-02-03 17:24:26 +0900

    Revert "Route / Link setting"
    This reverts commit 90cfcbd939c97490d84e2e07314209b0ecfc9fac.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
-import { Switch, Route, withRouter } from 'react-router-dom';
-import Header from './components/Header';
-import PostList from './components/PostList';
-import PostDetails from './components/PostDetails';
-import AddPost from './components/AddPost';
-import PostEdit from './components/PostEdit';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-
-    render() {
-        return (
-            <div>
-                <Header/>
-                <Switch>
-                    <Route exact path='/' component={PostList} />
-                    <Route exact path="/new" component={AddPost} />
-                    <Route exact path="/edit/:id" component={PostEdit} />
-                    <Route exact path="/:category" component={PostList} />
-                    <Route exact path="/:category/:id" component={PostDetails} />
-                </Switch>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
 
-export default withRouter(App)
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import configureStore from './configureStore';
-import App from  './App';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-const store = configureStore()
-
-ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter><App /></BrowserRouter>
-    </Provider>
-    , document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
