eb50c54a61efab3e921bce807fc35b633205111c
randomness38 <randomness.jay@gmail.com>
2018-02-05 16:14:55 +0900

    Vote control works!

--- src/components/Forms/PostControl.js
+++ src/components/Forms/PostControl.js
@@ -8,21 +8,38 @@ class PostControl extends Component {
 
     render () {
 
-        const { post, onDeletePost } = this.props;
+        const { post, onDeletePost, onSndVote } = this.props;
         // const { post } = this.props;
         console.log(post.id)
 
         return (
             <div>
                 <div>
+                    {/*VOTE PLACE*/}
+                    <div>
+                        <i onClick={(e) => {
+                            e.preventDefault();
+                            onSndVote(post.id,"upVote")
+                        }}
+                          >   UP  </i>
+
+                        <span>{post.voteScore}</span>
+
+                        <i onClick={(e) => {
+                            e.preventDefault();
+                            onSndVote(post.id,"downVote")
+                        }}
+                        >   DOWN  </i>
+
+                    </div>
+
                     <Link to={`/edit/post/${post.id}`}>
                         <button>Edit Button</button>
                     </Link>
-                    {/* 버튼이 자동으로 클릭돼서 모든 포스트가 다 삭제되는 버그 발생 중 */}
-
 
                     <button
                         onClick={ e => {
+                            // 이거 안하면 post 싹다 삭제 됨!
                             e.preventDefault();
                             onDeletePost(post.id)
                         }}
